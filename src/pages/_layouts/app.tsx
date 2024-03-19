34e1bfb7110f8150edfb448a53834957cd22fab6
Victor Vilela Duarte <victorvduarte@gmail.com>
2024-03-19 02:13:44 -0300

    creating menu and sign-up page

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,22 +1,14 @@
-import { Outlet } from "react-router-dom";
-import { Pizza } from "lucide-react";
+import { Outlet } from 'react-router-dom'
+
+import { Header } from '@/components/header'
 
 export function AppLayout() {
   return (
-    <div className="min-h-screen grid-cols-2">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
-        <div className="flex items-center gap-3 text-lg text-foreground">
-          <Pizza className="h-5 w-5" />
-          <span className="font-semibold">pizza.shop</span>
-        </div>
-        <footer className="text-sm">
-          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
-        </footer>
-      </div>
-      <h1>CABEÇALHO</h1>
-      <div>
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
-  );
+  )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,23 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom'
 
-import { Dasboard } from "./pages/app/dashboard";
-import { SignIn } from "./pages/auth/sign-in";
-import { AppLayout } from "./pages/_layouts/app";
-import { AuthLayout } from "./pages/_layouts/auth";
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+import { Dasboard } from './pages/app/dashboard'
+import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <AppLayout />,
-    children: [{ path: "/", element: <Dasboard /> }],
+    children: [{ path: '/', element: <Dasboard /> }],
   },
   {
-    path: "/",
+    path: '/',
     element: <AuthLayout />,
-    children: [{ path: "/sign-in", element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
   },
-]);
+])
