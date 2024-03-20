87e774ebfea124873ed5defd6013b37164ebefb4
Victor Vilela Duarte <victorvduarte@gmail.com>
2024-03-20 02:05:38 -0300

    pagination component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ export function Header() {
     <div className="border-b">
       <div className="flex h-16 items-center gap-6 px-6">
         <Pizza className="h-6 w-6" />
-        <Separator orientation="vertical" classname="h-6" />
+        <Separator orientation="vertical" className="h-6" />
         <nav className="flex items-center space-x-4 lg:space-x-6">
           <NavLink to="/">
             <Home className="h-4 w-4" />
